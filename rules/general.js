9323252aedf1dc13f6a685899bc97e7e1d4f0239
Willem-Jaap <willemjaapk@gmail.com>
2023-03-31 11:10:08 +0200

    fix: Add general rules to rules object, otherwise they are not loaded.

--- rules/general.js
+++ rules/general.js
@@ -1,4 +1,6 @@
 module.exports = {
-    "color-no-invalid-hex": true,
-    "selector-class-pattern": "^[a-z]+([-]{1,2}[a-z]+|[_]{2}[a-z]+)*$",
+    rules: {
+        "color-no-invalid-hex": true,
+        "selector-class-pattern": "^[a-z]+([-]{1,2}[a-z]+|[_]{2}[a-z]+)*$",
+    },
 };
